843427105bbba625a4b6737edde52d0942356cb8
Juan Pablo Botero <juanpabotero1@gmail.com>
2023-01-25 13:37:54 -0500

    feat: add register page

--- src/app/app.-routing.module.ts
+++ src/app/app.-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then((m) => m.RegisterModule),
+  },
   { path: '**', redirectTo: '' },
 ];
 
